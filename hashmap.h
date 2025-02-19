facb824c2c4b5cea5e81b7ae1e85a9a7dad89499
Arthur Maronian <contact@arthur-maronian.fr>
2025-02-19 13:42:50 +0100

    Début hashmap

--- hashmap.h
+++ hashmap.h
@@ -1,6 +1,8 @@
 #ifndef HASHMAP
 #define HASHMAP
 #define TOMBSTONE (( void *) -1)
+#define TABLE_SIZE 128
+
 typedef struct hashentry {
     char * key ;
     void * value ;
@@ -9,4 +11,10 @@ typedef struct hashmap {
     int size ;
     HashEntry * table ;
 } HashMap ;
+unsigned long simple_hash(const char *str);
+HashMap *hashmap_create();
+int hashmap_insert(HashMap *map, const char *key, void *value);
+void *hashmap_get(HashMap *map, const char *key);
+int hashmap_remove(HashMap *map, const char *key);
+void hashmap_destroy(HashMap *map);
 #endif
