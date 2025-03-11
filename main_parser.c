7ca13e774ebf07433ca48e45a302cfcebd2aa500
Matéo Boury <matboury@proton.me>
2025-03-11 21:08:45 +0100

    Ajout de tests

--- main_parser.c
+++ main_parser.c
@@ -6,9 +6,13 @@
 int main() {
     ParserResult* parser = parse("test_assert.txt");
     
-    assert(strcmp(parser->data_instructions[0]->mnemonic,"X")==0);
+    assert(strcmp(parser->data_instructions[0]->mnemonic,"x")==0);
+    assert(strcmp(parser->data_instructions[0]->operand1,"DW")==0);
+    assert(strcmp(parser->data_instructions[0]->operand2,"42")==0);
     assert(strcmp(parser->code_instructions[0]->mnemonic,"MOV")==0);
-
+    assert(strcmp(parser->code_instructions[0]->operand1,"AX")==0);
+    assert(strcmp(parser->code_instructions[0]->operand2,"x")==0);
+    
     free_parser_result(parser);
     return 0;
 }
