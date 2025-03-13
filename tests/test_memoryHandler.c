eb77e1e3be418958c2f180aaa94cc78b17bfb952
Matéo Boury <matboury@proton.me>
2025-03-13 08:52:01 +0100

    Test hashmap

--- tests/test_memoryHandler.c
+++ tests/test_memoryHandler.c
@@ -5,7 +5,7 @@
 
 int main(){
     MemoryHandler *memo = memory_init(1000);
-    Segment *border1, *border2, *border3, *parcours;
+    Segment *border1, *border2, *border3, *parcours, *seg=NULL;
     border1=(Segment *)malloc(sizeof(Segment));
     border1->next=NULL;
     border1->size=100;
@@ -25,6 +25,10 @@ int main(){
     create_segment(memo,"border1", border1->start, border1->size);
     create_segment(memo,"border2", border2->start, border2->size);
     create_segment(memo,"border3", border3->start, border3->size);
+
+    seg=hashmap_get(memo->allocated, "border1");
+    assert(border1->start==seg->start);
+    assert(border1->size==seg->size);
     
     parcours=memo->free_list;
     assert(parcours->start==border1->start+border1->size);
