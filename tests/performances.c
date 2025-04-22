1ca7641fd98e5d65b0b03a1ebe3746695afe22fe
Matéo Boury <matboury@proton.me>
2025-04-22 09:23:22 +0200

    ajout de perf pour parser et cpu + reduction du nombre de valeur pour hashmap et memoryhandler

--- tests/performances.c
+++ tests/performances.c
@@ -1,3 +1,4 @@
+#include <bits/types/clock_t.h>
 #include <time.h>
 #include <stdio.h>
 #include "../hashmap.h"
@@ -83,6 +84,29 @@ void test_memory_handler(FILE *f, int n) {
     free_memory_handler(mem);
 }
 
+void test_parser_and_cpu(FILE *f_parser, FILE *f_cpu, const char * file_input_name, int n) {
+    clock_t init_parse = clock();
+    ParserResult *pr = parse(file_input_name);
+    clock_t end_parse = clock();
+
+    CPU *cpu = cpu_init(2*(n+128)); //+128 car si n<128, alors on ne peut pas allouer de Stack Segment
+    clock_t init_allocate = clock();
+    allocate_variables(cpu, pr->data_instructions, pr->data_count);
+    clock_t end_allocate = clock();
+
+    clock_t init_free_parser = clock();
+    free_parser_result(pr);
+    clock_t end_free_parser = clock();
+
+    clock_t init_free_cpu = clock();
+    cpu_destroy(cpu);
+    clock_t end_free_cpu = clock();
+
+    fprintf(f_parser, "%d %.6f %.6f\n", n, (double) (end_parse-init_parse)/CLOCKS_PER_SEC, (double) (end_free_parser-init_free_parser)/CLOCKS_PER_SEC);
+
+    fprintf(f_cpu, "%d %.6f %.6f\n", n, (double) (end_allocate-init_allocate)/CLOCKS_PER_SEC, (double) (end_free_cpu-init_free_cpu)/CLOCKS_PER_SEC);
+}
+
 int main() {
     FILE *f1, *f2, *f3, *f4;
     /*On initialise des variables FILE pour chaque fichiers de temps*/
@@ -91,11 +115,18 @@ int main() {
     f3=fopen("out_parser.txt","w");
     f4=fopen("out_cpu.txt","w");
     printf("Program initialized\n");
-    for(int i=1; i<100; i++) {
-        printf("i : %d\n", i);
-        test_hash(f1, i*200);
-        test_memory_handler(f2, i*200);
+    for(int i=1; i<10; i++) {
+        printf("Value : %d\n", i*2000);
+        test_hash(f1, i*2000);
+        test_memory_handler(f2, i*2000);
     }
+    
+    //Le dernier parametre est le nombre de lignes maximal entre la partie .DATA et .CODE
+    test_parser_and_cpu(f3, f4, "tests/test_parser.txt", 3);
+    test_parser_and_cpu(f3, f4, "tests/perf_parser1000.txt", 1152);
+    test_parser_and_cpu(f3, f4, "tests/perf_parser2000.txt", 2304);
+    test_parser_and_cpu(f3, f4, "tests/perf_parser4000.txt", 4608);
+    test_parser_and_cpu(f3, f4, "tests/perf_parser8000.txt", 9216);
     printf("Program ended\n");
     fclose(f1);
     fclose(f2);
